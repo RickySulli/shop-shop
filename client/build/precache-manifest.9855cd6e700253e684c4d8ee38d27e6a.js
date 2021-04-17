3c0b2bb1a381c046ae5f415e2cd70e90d6764fa8
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-04-16 21:17:30 -0500

    herrrrr

---
+++ client/build/precache-manifest.9855cd6e700253e684c4d8ee38d27e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fead94217a86dd61d508ae13d0828284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81fcbba2da1075a941dd",
+    "url": "/static/css/main.7a843e87.chunk.css"
+  },
+  {
+    "revision": "c0631a8bfa4ae43d91a8",
+    "url": "/static/js/2.e95c7b76.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e95c7b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fcbba2da1075a941dd",
+    "url": "/static/js/main.2d04eb16.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);

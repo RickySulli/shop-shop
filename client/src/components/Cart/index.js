1596ea237da919fc2e7e2a2553698b427f7d08a5
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-04-16 20:18:36 -0500

    22/4.6

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,9 +4,17 @@ import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext }from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import {idbPromise} from "../../utils/helpers";
+import { QUERY_CHECKOUT} from '../../utils/queries';
+import { loadStripe} from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { idbPromise } from '../../utils/helpers';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 
 const Cart = () => {
+  
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
     const [state, dispatch] = useStoreContext();
     useEffect(() => {
       async function getCart() {
@@ -18,7 +26,13 @@ const Cart = () => {
       }
     }, [state.cart.length, dispatch]);
     console.log(state);
-
+    useEffect(() => {
+      if (data) {
+        stripePromise.then((res) => {
+          res.redirectToCheckout({ sessionId: data.checkout.session });
+        });
+      }
+    }, [data]);
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
@@ -38,6 +52,20 @@ const Cart = () => {
         });
         return sum.toFixed(2);
     }
+    function submitCheckout() {
+      const productIds = [];
+      getCheckout({
+        variables: { products: productIds }
+      });
+    
+      state.cart.forEach((item) => {
+        for (let i = 0; i < item.purchaseQuantity; i++) {
+          productIds.push(item._id);
+        }
+      });
+    }
+ 
+
   return (
 <div className="cart">
   <div className="close" onClick={toggleCart}>[close]</div>
@@ -51,16 +79,13 @@ const Cart = () => {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick = {submitCheckout}>
               Checkout
             </button>
             :
             <span>(log in to check out)</span>
         }
       </div>
-      <button role="link">
-      Checkout
-    </button>
     </div>
   ) : (
     <h3>
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const resolvers = {
   Query: {
     categories: async () => {
@@ -83,7 +83,7 @@ const resolvers = {
           line_items,
           mode: 'payment',
           success_url: `${url}/success?session_id=cs_test_a1tdVTE5jzqWeiVv0dKNLiHYHn3RheSjmu6lLzJDLycwuPr1rmw8IGVT4a`,
-          cancel_url: 'https://example.com/cancel'
+          cancel_url: `${url}/`
         });
         
         return { session: session.id };
